93b035e0a4a9b0e867524f4d2d580f3cfc1f6558
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2023-09-17 21:42:57 +0530

    tenth commit

--- src/Components/NavbarComponents/FourthNavbar.jsx
+++ src/Components/NavbarComponents/FourthNavbar.jsx
@@ -1,4 +1,11 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "../NavbarComponents/FourthNav.css";
+import NewOffImg1 from "/src/assets/Offimg/Offimg1.jpg";
+import NewOffImg2 from "/src/assets/Offimg/Offimg2.webp";
+import NewOffImg3 from "/src/assets/Offimg/Offimg3.avif";
+import NewOffImg4 from "/src/assets/Offimg/Offimg4.jpg";
+import NewOffImg5 from "/src/assets/Offimg/Offimg5.webp";
+import NineComp from "../Ninecomp/NineComp";
 
 const FourthNavbar = () => {
   return (
@@ -6,17 +13,23 @@ const FourthNavbar = () => {
       <section className="fourth-nav-sec">
         <nav className="navbar navbar-expand-lg fourth-nav">
           <div className="container-fluid ">
-            <a className="navbar-brand" style={{textDecoration:"none", color:"white"}}href="#">
+            <a
+              className="navbar-brand"
+              style={{ textDecoration: "none", color: "white" }}
+              href="#"
+            >
               Aesop
             </a>
             <span className="fourth-nav-a  d-flex ms-auto">
-              <a href="" style={{color:'white'}}>
+              <a href="" style={{ color: "white" }}>
                 <i className="bi bi-search"></i>
               </a>
               <a href="" style={{ color: "white" }}>
                 <i className="bi bi-heart"></i>
               </a>
-              <a href="" style={{color:"white"}}>Cart</a>
+              <a href="" style={{ color: "white" }}>
+                Cart
+              </a>
             </span>
             <button
               className="navbar-toggler"
@@ -26,7 +39,7 @@ const FourthNavbar = () => {
               aria-controls="offcanvasDarkNavbar"
               aria-label="Toggle navigation"
             >
-              <h3 style={{color:"white"}}>=</h3>
+              <h3 style={{ color: "white" }}>=</h3>
             </button>
             <div
               className="offcanvas offcanvas-top text-bg-white"
@@ -35,9 +48,26 @@ const FourthNavbar = () => {
               id="offcanvasDarkNavbar"
             >
               <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                  Dark offcanvas
+                <h5
+                  className="offcanvas-title"
+                  id="offcanvasDarkNavbarLabel"
+                  style={{
+                    color: "#333",
+                    fontSize: "16px",
+                    fontFamily: "Sussie,snas-serif",
+                  }}
+                >
+                  Aesop
                 </h5>
+                <span className="fourth-off-icon">
+                  <a href="">
+                    <i className="bi bi-search"></i>
+                  </a>
+                  <a href="">
+                    <i className="bi bi-heart"></i>
+                  </a>
+                  <a href="">Cart</a>
+                </span>
                 <button
                   type="button"
                   className="btn-close btn-close-black"
@@ -48,7 +78,7 @@ const FourthNavbar = () => {
               <div className="offcanvas-body">
                 <ul className="navbar-nav fourth-off-nav justify-content-end flex-grow-1 pe-3">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a className="nav-link" aria-current="page" href="#">
                       Skin Care
                     </a>
                   </li>
@@ -83,11 +113,145 @@ const FourthNavbar = () => {
                     </a>
                   </li>
                 </ul>
+                <span className="fourth-off-span-nav">
+                  <a href="#2">Read</a>
+                  <a href="#2">Log in</a>
+                  <a href="#2">Stores</a>
+                  <a href="#2">Live assistance</a>
+                  <a href="#2">Facial Appointments</a>
+                </span>
+                <div className="fourth-off-cars">
+                  <Splide
+                    className="fourth-off-cars1"
+                    options={{
+                      gap: "8rem",
+                      pagination: 0,
+                      dots: false,
+                      focus: 0,
+                      loop: true,
+                    }}
+                  >
+                    <SplideSlide className="fourth-off-cars2">
+                      <div
+                        className="card fourth-off-card"
+                        style={{ borderRadius: "0%" }}
+                      >
+                        <img
+                          src={NewOffImg1}
+                          className="card-img-top fourth-off-img"
+                          alt="..."
+                        />
+                        <div className="card-body fourth-off-card-body">
+                          <h6 className="card-title fourth-off-h6">
+                            Lessons from the lab
+                          </h6>
+                          <p
+                            className="card-text fouth-off-p"
+                            style={{
+                              fontSize: "13px",
+                              fontFamily: "Sussie ,sans-serif",
+                            }}
+                          >
+                            The next chapter in Aesop fragrance: Gloam Eau de
+                            Parfum
+                          </p>
+                        </div>
+                      </div>
+                    </SplideSlide>
+                    <SplideSlide className="fourth-off-cars2">
+                      <div className="card fourth-off-card">
+                        <img
+                          src={NewOffImg2}
+                          className="card-img-top"
+                          alt="..."
+                        />
+                        <div className="card-body  fourth-off-card-body">
+                          <h6 className="card-title fourth-off-h6">Habitat</h6>
+                          <p
+                            className="card-text fourth-off-p"
+                            style={{
+                              fontSize: "13px",
+                              fontFamily: "Sussie ,sans-serif",
+                            }}
+                          >
+                            A guide to home fragrance
+                          </p>
+                        </div>
+                      </div>
+                    </SplideSlide>
+                    <SplideSlide className="fourth-off-cars2">
+                      <div className="card fourth-off-card">
+                        <img
+                          src={NewOffImg3}
+                          className="card-img-top"
+                          alt="..."
+                        />
+                        <div className="card-body fourth-off-card-body">
+                          <h6 className="card-title fourth-off-h6">
+                            Lesson from the lab
+                          </h6>
+                          <p
+                            className="card-text fourth-off-p"
+                            style={{
+                              fontSize: "13px",
+                              fontFamily: "Sussie ,sans-serif",
+                            }}
+                          >
+                            How to care for the delicate skin around the eyes
+                          </p>
+                        </div>
+                      </div>
+                    </SplideSlide>
+                    <SplideSlide className="fourth-off-cars2">
+                      <div className="card fourth-off-card">
+                        <img
+                          src={NewOffImg4}
+                          className="card-img-top"
+                          alt="..."
+                        />
+                        <div className="card-body fourth-off-card-body">
+                          <h6 className="card-title fourth-off-h6">Podcast</h6>
+                          <p
+                            className="card-text fourth-off-p"
+                            style={{
+                              fontSize: "13px",
+                              fontFamily: "Sussie ,sans-serif",
+                            }}
+                          >
+                            Radiomatique Future Fables
+                          </p>
+                        </div>
+                      </div>
+                    </SplideSlide>
+                    <SplideSlide className="fourth-off-cars2">
+                      <div className="card fourth-off-card">
+                        <img
+                          src={NewOffImg5}
+                          className="card-img-top"
+                          alt="..."
+                        />
+                        <div className="card-body fourth-off-card-body">
+                          <h6 className="card-title fourth-off-h6">Culture</h6>
+                          <p
+                            className="card-text fourth-off-p"
+                            style={{
+                              fontSize: "13px",
+                              fontFamily: "Sussie ,sans-serif",
+                            }}
+                          >
+                            Musings from the Rain Garden
+                          </p>
+                        </div>
+                      </div>
+                    </SplideSlide>
+                  </Splide>
+                </div>
               </div>
             </div>
           </div>
         </nav>
       </section>
+      <NineComp/>
     </>
   );
 };
---
+++ src/Components/Ninecomp/NineComp.jsx
@@ -0,0 +1,23 @@
+import "../Ninecomp/NineComp.css"
+
+const NineComp = () => {
+  return (
+    <>
+      <section className="nine-section">
+        <h3 className="nine-h3">Doorways to the undiscovered</h3>
+        <div className="nine-detail">
+          <h3>Othertopias: a collection of unorthodox fragrances</h3>
+          <p>
+            Step inside six fragrant worlds that transcend our immediate
+            perception-vivid and distinctive{" "}
+          </p>
+          <a href className="nine-a">
+            Discover the frgarance <i className="bi bi-arrow-right" style={{color:"#C7F0F2"}}></i>
+          </a>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default NineComp

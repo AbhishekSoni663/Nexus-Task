bbec88270acb270e3c1f01e0f0221319ee301885
AbhishekSoni663 <Abhisheksoni663@gmail.com>
2023-09-17 16:47:58 +0530

    seventh commit

--- src/Components/Notifaction/Notification.jsx
+++ src/Components/Notifaction/Notification.jsx
@@ -6,7 +6,7 @@ const Notification = () => {
       {/* <div className="notification">
      <a className="noti-icon" href=""></a>
    </div> */}
-      <a className="noti-icon" href="" style={{border:"1px solid white", width:"42px",borderRadius:"50%",padding:"6px", display:"flex",alignItems:"center",justifyContent:"center",position:"fixed"}}>
+      <a className="noti-icon" href="" style={{borderRadius:"50%", display:"flex",alignItems:"center",justifyContent:"center",position:"fixed"}}>
         <i className="bi bi-chat-right"></i>
       </a>
     </>
